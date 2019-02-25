39e8b25a38e67a9f7e0b462acd4e4ad01551cdb8
IvanDaza <talivan0717@hotmail.com>
2019-02-25 08:07:45 -0500

    Todo List finished

---
+++ Task.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model; // Importando la libreria Eloquent.
+
+class Task extends Model{
+    protected $table = 'tasks'; // como practica le ponemos la tabla que queremos que utilice 
+}
